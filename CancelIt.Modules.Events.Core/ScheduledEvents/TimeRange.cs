c3d13ea30ddb8b30e38c70ef02d1f803d1cb3b21
Adam Praszywka <praszywka.adam@gmail.com>
2024-07-14 19:39:00 +0200

    Add TimeRange ValueObject

---
+++ CancelIt.Modules.Events.Core/ScheduledEvents/TimeRange.cs
@@ -0,0 +1,27 @@
+﻿namespace CancelIt.Modules.Events.Core.ScheduledEvents;
+
+public readonly struct TimeRange
+{
+    public TimeRange(DateTimeOffset start, DateTimeOffset end)
+    {
+        var duration = end - start;
+        if (duration == TimeSpan.Zero)
+        {
+            throw new ArgumentException("Duration must be greater than zero");
+        }
+        
+        if (duration < TimeSpan.Zero)
+        {
+            throw new ArgumentException("Start date must be before end date");
+        }
+        
+        Start = start;
+        End = end;
+    }
+    
+    
+    public DateTimeOffset Start { get; }
+    public DateTimeOffset End { get; }
+    
+    public TimeSpan Duration => End - Start;
+}
---
+++ CancelIt.Modules.Events.CoreTests/ScheduledEvents/TimeRangeTests.cs
@@ -0,0 +1,40 @@
+﻿using CancelIt.Modules.Events.Core.ScheduledEvents;
+using NUnit.Framework;
+
+namespace CancelIt.Modules.Events.CoreTests.ScheduledEvents;
+
+public class TimeRangeTests
+{
+    [Test]
+    public void ValidTimeRange()
+    {
+        var start = new DateTimeOffset(2024, 06, 06, 1, 0, 0, TimeSpan.Zero);
+        var end = new DateTimeOffset(2024, 06, 06, 2, 0, 0, TimeSpan.Zero);
+        
+        var timeRange = new TimeRange(start, end);
+        Assert.Multiple(() =>
+        {
+            Assert.That(timeRange.Start, Is.EqualTo(start));
+            Assert.That(timeRange.End, Is.EqualTo(end));
+            Assert.That(timeRange.Duration, Is.EqualTo(TimeSpan.FromHours(1)));
+        });
+    }
+
+    [Test]
+    public void ZeroDuration()
+    {
+        var start = new DateTimeOffset(2024, 06, 06, 1, 0, 0, TimeSpan.Zero);
+        var end = new DateTimeOffset(2024, 06, 06, 1, 0, 0, TimeSpan.Zero);
+        
+        Assert.That(() => new TimeRange(start, end), Throws.ArgumentException);
+    }
+    
+    [Test]
+    public void NegativeDuration()
+    {
+        var start = new DateTimeOffset(2024, 06, 06, 2, 0, 0, TimeSpan.Zero);
+        var end = new DateTimeOffset(2024, 06, 06, 1, 0, 0, TimeSpan.Zero);
+        
+        Assert.That(() => new TimeRange(start, end), Throws.ArgumentException);
+    }
+}
